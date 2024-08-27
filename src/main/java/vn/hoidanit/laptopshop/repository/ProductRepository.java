12ab454549cda19391b0e79136cabb64633c3ac6
TuanLeSon <78398071+TuanLeSon@users.noreply.github.com>
2024-08-27 16:02:40 +0700

    #142

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,5 @@ import vn.hoidanit.laptopshop.domain.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product save(Product product);
 
-    // Product findById(long id);
+    Page<Product> findAll(Pageable page);
 }
--- src/main/webapp/WEB-INF/view/admin/order/show.jsp
+++ src/main/webapp/WEB-INF/view/admin/order/show.jsp
@@ -69,6 +69,7 @@
                                                         </c:forEach>
                                                     </tbody>
                                                 </table>
+
                                             </div>
                                         </div>
                                     </div>
